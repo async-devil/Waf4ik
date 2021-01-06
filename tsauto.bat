d19e6e3c7bf62ca9225e3a5207f55aeae09c97c6
async-devil <async.devil@gmail.com>
2021-01-06 22:01:51 +0200

    Added start scripts

---
+++ tsauto.bat
@@ -0,0 +1,1 @@
+npx tsc --watch ./public/ts/main.ts --outDir ./public/js -t "es6" --sourceMap true -m "es2020"
---
+++ tsauto.sh
@@ -0,0 +1,1 @@
+npx tsc --watch ./public/ts/*.ts --outDir ./public/js -t "es6" --sourceMap true -m "es2020"
